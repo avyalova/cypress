f9fe78900d8a9c7d90bb992a273689dc8677bfb4
Anna <anna_vyalova@yahoo.com>
2023-03-04 16:00:52 +0100

    15-1

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,8 @@
 const { defineConfig } = require("cypress")
 
-module.exports = {}
-defineConfig({
+module.exports = defineConfig({
   projectId: "x7vjsr",
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
   },
 })
